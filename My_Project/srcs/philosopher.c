b6d3927c7e0e27e4ece6b0d63ca3869f1bfb7e35
Uatilla <uatillaalmeida@gmail.com>
2024-03-19 17:23:35 +0000

    Logic under the thread.

--- My_Project/srcs/philosopher.c
+++ My_Project/srcs/philosopher.c
@@ -14,7 +14,25 @@
 
 static void *lone_philo_routine(t_philo *philo)
 {
-    pthread_mutex_lock(&)
+    /*
+    1) TRY TO EAT:
+        
+    LOCK FORK
+
+        Take the fork
+            How to assign the fork to the philo?
+            How to assure that this fork is not available for others?
+            How to write on the screen that a fork was pick?
+        Try to EAT since has only one fork, wait for another fork.
+            How to assure that the philo will not eat since he doesn't have 
+            two forks available.
+    2) DIE:
+    UNLOCK FORK
+        How to assure that the philo will die since the die time was elapsed?
+        How to return the feedback to the terminal that the philo died inside the time available
+            (10 ms).
+        Return
+    */
     return (NULL);
 }
 
@@ -28,6 +46,9 @@ void    *philosopher(void *data)
     if (philo->table->max_meals == 0)
         return (NULL);
     pthread_mutex_lock(&philo->meal_time_locker);
+    /*Considering that this variable below is not shared with all threads, 
+    since the current var is inside the structure of the philo itself, it should
+    use lock to write on it?*/
     philo->last_meal_time = philo->table->start_time;
     pthread_mutex_unlock(&philo->meal_time_locker);
     while (get_ms_time() < philo->table->start_time)
